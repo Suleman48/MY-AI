b00a3d4eabc1c60272c23a7aeffad75405a13b51
Suleman <sulemanshoukat48@gmail.com>
2026-02-13 16:47:21 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+print ("Suleman")
+
+E = 12
+H = 5
+A = 20
+
+
+if education > 11 and height > 4.9
+   print("Eligible")
+elif: education== 12 and height < 5 and age < 20
+   print("You are eliglbe")
+elif: education > 12 and height== 5 and age < 20
+   print("u eligable")
+else: 
+   print ("fail")
